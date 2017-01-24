bb4f5be286bb5674c25b5abed37d717ac82353a1
kimdhoe <kimdhoe1@gmail.com>
2017-01-24 18:12:28 +0900

    style(lint): standard

--- src/fillInJosa.js
+++ src/fillInJosa.js
@@ -3,9 +3,10 @@ import { get } from './josaPicker'
 // fillInJosa : string -> string
 // Fills in the josa placeholders: 친구#{이} 학교#{으로} -> 친구가 학교로
 const fillInJosa = str =>
-             /*  +++(___ ____ ____)#{---------} */
-  str.replace( /(\S+(?:\([^)]*\))?)#\{([^}]+)\}/g
-             , (_, noun, josa) => noun + get(josa.trim())(noun)
-             )
+  str.replace(
+   /* +++(___ ____ ____)#{---------} */
+    /(\S+(?:\([^)]*\))?)#\{([^}]+)\}/g,
+    (_, noun, josa) => noun + get(josa.trim())(noun)
+  )
 
 export default fillInJosa
